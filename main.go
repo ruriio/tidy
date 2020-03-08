ca0f2eca09a5fb58826deae051077a58b7b31f25
Gokou Ruri <gokou@ruri.io>
2020-03-08 21:58:36 +0800

    add param parser

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
 
+	version := flag.Bool("version", false, "prints current version")
+	v := flag.Bool("v", false, "prints current version")
+	flag.Parse()
+
+	if *version || *v {
+		fmt.Println("Version: 1.0.0")
+		os.Exit(0)
+	}
 }
